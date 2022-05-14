7921d0a10ebe09889abaf8832030a9d40ad57dc4
Fabalin <williamlin.ygn@gmail.com>
2022-05-14 16:19:55 -0700

    Updating with a marker

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,13 +7,20 @@ console.log("working");
 // Create the map object with a center and zoom level for more control and customization.
 let map = L.map("mapid", {
     center: [
-      40.7, -94.5
+      34.0522, -118.2437
     ],
-    zoom: 4
+    zoom: 14
 });
 
+// Add a circle marker to the map for Los Angeles, CA
+L.circle([34.0522, -118.2437], {
+    radius: 300, 
+    color: "black",
+    fillColor: '#ffffa1'
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
     // Link to more mapbox styles https://docs.mapbox.com/api/maps/styles/
     // Link to change styles https://docs.mapbox.com/api/maps/static-tiles/
@@ -23,3 +30,7 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
 
 // Then we add our 'graymap' tile layer to the map.
 streets.addTo(map);
+
+//  Add a marker to the map for Los Angeles, California.
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
